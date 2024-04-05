1bf8c268cc3b1b9eb31780743f45fe3bb0cc6f74
hugoht3 <hugoht3@hotmail.com>
2024-04-05 13:01:37 +0100

    started in js with a simple function

--- assests/js/style.js
+++ assests/js/style.js
@@ -1,1 +1,10 @@
-console.log("TRIAL TRIAL TRIAL");
+function log () {
+    let userInput = document.getElementById("answer-box1").value;
+    let userInput2 = document.getElementById("answer-box2").value;
+    let userInput3 = document.getElementById("answer-box3").value;
+    let userInput4 = document.getElementById("answer-box4").value;
+    console.log(userInput * userInput2 * userInput3 * userInput4);
+
+
+
+}
